c2e0992d3cc5d9a5ce506f1b43bf88bb72f557af
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-11-19 12:57:35 -0500

    ORM completed

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,59 @@
 // Import MySQL Connection
 const connection = require('./connection');
 
+// Configured the 'Object-Relational Mapper' for making queries to the database - defining our functions and parameters.
 const orm = {
-    selectAll,
-    insertOne,
-    updateOne,
+
+    // Query to retrieve all of the sushi information in the database.
+    selectAll: function(cb) {
+        const queryAll = 'SELECT * FROM sushi';
+        connection.query(queryAll, function(err, results) {
+            if (err) throw err;
+            cb(results);
+        });
+    },
+
+    // Query to insert a new sushi into the database, it receives the name of the sushi from the model.
+    insertOne: function(cols, num, cb) {
+        const insertQuery = `INSERT INTO sushi (${cols.toString()}) VALUES (${questionMarks(num)})`;
+        connection.query(insertQuery, function(err, result) {
+            if (err) throw err;
+            cb(result)
+        });
+    },
+
+    // Function to update
+    updateOne: function(updateSushiObj, id, cb) {
+        const updateQuery = `UPDATE sushi SET ${colToEqual(updateSushiObj)} WHERE ${id}`;
+        connection.query(updateQuery, function(err, result) {
+            if (err) throw err;
+            cb(result)
+        });
+    }
 };
+
+function questionMarks(num) {
+    let marks = [];
+
+    for (let k = 0; k < num; k++) {
+        marks.push('?')
+    }
+}
+
+function colToEqual(obj) {
+    let equals = [];
+
+    for (let key in obj) {
+        let value = obj[key];
+        if (Object.hasOwnProperty.call(obj, key)) {
+            if (typeof value === 'string' && value.indexOf(" ") >= 0) {
+                value = `'${value}'`
+            }
+            equals.push(`${key}=${value}`)
+        }
+    }
+    return equals.toString()
+}
 
 // Export the orm object for the model. (sushi.js)
 module.exports = orm;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,12 @@
 USE sushi_db;
 
-INSERT INTO sushi (sushi_name, devoured)
-VALUES ('Ankimo', FALSE),
-       ('Uni', FALSE),
-       ('Unagi', FALSE),
-       ('Masago', FALSE),
-       ('Toro', FALSE),
-       ('Uni with Quail Egg', FALSE),
+INSERT INTO sushi (sushi_name, mindfully_ate)
+VALUES ('Ankimo', 0),
+       ('Uni', 0),
+       ('Unagi', 0),
+       ('Masago', 0),
+       ('Toro', 0),
+       ('Uni with Quail Egg', 0),
        ('Masago with Quail Egg', 0),
        ('Ikura with Quail Egg', 0);
 
