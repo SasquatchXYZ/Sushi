8b8581abba3a19542935b675d6076ba7bd525eab
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-11-30 19:22:01 -0500

    troubleshooting

--- controllers/sushi_controller.js
+++ controllers/sushi_controller.js
@@ -14,7 +14,7 @@ router.get('/', function (req, res) {
         const hbsObject = {
             sushi: data
         };
-        //console.log(hbsObject);
+       // console.log(hbsObject);
         res.render('index', hbsObject);
     });
 });
